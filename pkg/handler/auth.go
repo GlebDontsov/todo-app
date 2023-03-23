8c189421401ddb9a5bac0344f2288b18c29827f9
GlebDontsov <dontsovggwp@mail.ru>
2023-03-23 17:24:03 +0300

    add user registration

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,9 +1,27 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	todo "todo-app"
+)
 
 func (h *Handler) SignUp(c *gin.Context) {
+	var input todo.User
 
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+	}
+
+	id, err := h.services.Authorization.CreateUser(input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 }
 
 func (h *Handler) SignIn(c *gin.Context) {
---
+++ pkg/service/auth.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"crypto/sha1"
+	"fmt"
+	todo "todo-app"
+	"todo-app/pkg/repository"
+)
+
+const salt = "kwemrom1341234ekqfmqer"
+
+type AuthService struct {
+	repo repository.Authorization
+}
+
+func NewAuthService(repo repository.Authorization) *AuthService {
+	return &AuthService{repo}
+}
+
+func (s *AuthService) CreateUser(user todo.User) (int, error) {
+	user.Password = generatePasswordHash(user.Password)
+	return s.repo.CreateUser(user)
+}
+
+func generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+}
