38eb895f61e036768468b59ccc038f01faeb8a23
GlebDontsov <dontsovggwp@mail.ru>
2023-03-24 10:58:39 +0300

    add user authentication

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -6,11 +6,12 @@ import (
 	todo "todo-app"
 )
 
-func (h *Handler) SignUp(c *gin.Context) {
+func (h *Handler) signUp(c *gin.Context) {
 	var input todo.User
 
 	if err := c.BindJSON(&input); err != nil {
-		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		newErrorResponse(c, http.StatusBadRequest, "invalid input body")
+		return
 	}
 
 	id, err := h.services.Authorization.CreateUser(input)
@@ -24,6 +25,26 @@ func (h *Handler) SignUp(c *gin.Context) {
 	})
 }
 
-func (h *Handler) SignIn(c *gin.Context) {
+type signInInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
 
+func (h *Handler) signIn(c *gin.Context) {
+	var input signInInput
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
 }
